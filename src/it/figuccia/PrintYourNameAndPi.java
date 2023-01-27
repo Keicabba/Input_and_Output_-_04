6d454b526f335e432c04717baf6df085524088e0
Keicabba <peppino_cbr@hotmail.com>
2023-01-27 03:09:49 +0100

    Initial commit

---
+++ src/it/figuccia/PrintYourNameAndPi.java
@@ -0,0 +1,7 @@
+package it.figuccia;
+
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        System.out.printf("%S", "Pinco Pallino", "%n","The number Pi is: %f", 3.141590);
+    }
+}
